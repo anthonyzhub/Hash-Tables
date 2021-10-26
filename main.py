6be676be97fe79529b36c5427d4fd5dfe73fc268
Anthony <zamora.anthony1234@gmail.com>
2021-10-26 15:55:28 -0400

    List can automatically increase size

--- main.py
+++ main.py
@@ -7,6 +7,47 @@ class Dictionary:
         # Create an array with None values
         self.array = [None] * arrayLength
 
+    def isDictionaryFull(self):
+
+        # OBJECTIVE: Return boolean variable representing whether or not dictionary is nearly full
+
+        # Create counter
+        totalPairs = 0
+
+        # Iterate dictionary
+        for pair in self.array:
+
+            # If "pair" is NonEmpty, increment counter
+            if pair:
+                totalPairs += 1
+
+        # Return true if only 1 new key is needed to make the list full
+        return totalPairs >= len(self.array) - 1
+
+    def increaseSize(self):
+
+        # OBJECTIVE: Increase dictionary's size by half
+
+        print("Doubling size!")
+
+        # Initialize dictionary class from within
+        oldSize = len(self.array)
+        newArray = Dictionary(oldSize * 2)
+
+        # Iterate array
+        for idx in range(oldSize):
+
+            # If element at index is a NoneType, skip it
+            if self.array[idx]:
+
+                for elem in self.array[idx]:
+                    
+                    # Get original pair and add it to new list
+                    newArray.addPair(elem[0], elem[1])
+
+        # Update variable
+        self.array = newArray.array
+
     def hashStr(self, key):
 
         # OBJECTIVE: Return key's index based on its hash value and length of array
@@ -15,6 +56,10 @@ class Dictionary:
     def addPair(self, key, value):
 
         # OBJECTIVE: Add key and value pair to list
+
+        # If list is almost full, increase its size before moving on
+        if self.isDictionaryFull():
+            self.increaseSize()
 
         # Get key's potential index
         keyIdx = self.hashStr(key)
@@ -39,7 +84,10 @@ class Dictionary:
         # Execute if index inside list is empty
         else:
 
+            # Replace "None" with a list
             self.array[keyIdx] = list()
+
+            # Add values
             self.array[keyIdx].append([key, value])
 
     def getValue(self, key):
